9daa16807235ed9b5be1c179dce4ee9034dbd090
gem-tarunAg <tarun.agrawal@geminisolutions.com>
2025-07-11 09:26:30 +0530

    Morris Algorithm for InOrder Traversal

---
+++ BinaryTree/MorrisInOrder.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+class MorrisInOrder {
+
+    public static void morrisInOrder(TreeNode root, List<Integer> list){
+        if(root == null) {
+            return;
+        }
+
+        TreeNode curr = root;
+
+        while(curr != null) {
+            if(curr.left == null) {
+                list.add(curr.val);
+                curr = curr.right;
+            } else {
+                TreeNode prev = curr.left;
+                while(prev.right != null && prev.right != curr) {
+                    prev = prev.right;
+                }
+
+                if (prev.right == null) {
+                    prev.right = curr;
+                    curr = curr.left;
+                } else if (prev.right == curr) {
+                    list.add(curr.val);
+                    prev.right = null;
+                    curr = curr.right;
+                }
+            }
+        }
+    }
+
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the values for the tree nodes (space-separated): ");
+        String input = scanner.nextLine();
+        String[] values = input.split(" ");
+        TreeNode root = TreeNode.insertIntoBinaryTree(values, 0, values.length);
+
+        List<Integer> list = new ArrayList<>();
+        morrisInOrder(root, list);
+
+        for(Integer val: list){
+            System.out.print(val + " ");
+        }
+    }
+}
